da843d0d1574dca9ac0120c05226b5c72d39be26
Gordon Moore <gmgit@gordonmoore.co.uk>
2024-06-28 19:12:45 +0100

    Removed stuff from contacts_model.py

--- contacts_model.py
+++ contacts_model.py
@@ -1,9 +1,4 @@
 import json
-from operator import attrgetter
-import time
-from threading import Thread
-from random import random
-
 
 # ========================================================
 # Contact Model
@@ -57,11 +52,6 @@ class Contact:
         Contact.save_db()
 
     @classmethod
-    def count(cls):
-        time.sleep(2)
-        return len(cls.db)
-
-    @classmethod
     def all(cls, page=1):
         page = int(page)
         start = (page - 1) * PAGE_SIZE
@@ -102,44 +92,3 @@ class Contact:
             c.errors = {}
 
         return c
-
-
-class Archiver:
-    archive_status = "Waiting"
-    archive_progress = 0
-    thread = None
-
-    def status(self):
-        return Archiver.archive_status
-
-    def progress(self):
-        return Archiver.archive_progress
-
-    def run(self):
-        if Archiver.archive_status == "Waiting":
-            Archiver.archive_status = "Running"
-            Archiver.archive_progress = 0
-            Archiver.thread = Thread(target=self.run_impl)
-            Archiver.thread.start()
-
-    def run_impl(self):
-        for i in range(10):
-            time.sleep(1 * random())
-            if Archiver.archive_status != "Running":
-                return
-            Archiver.archive_progress = (i + 1) / 10
-            print("Here... " + str(Archiver.archive_progress))
-        time.sleep(1)
-        if Archiver.archive_status != "Running":
-            return
-        Archiver.archive_status = "Complete"
-
-    def archive_file(self):
-        return 'contacts.json'
-
-    def reset(self):
-        Archiver.archive_status = "Waiting"
-
-    @classmethod
-    def get(cls):
-        return Archiver()
