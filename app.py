9c81d85061576ad533eb561870df2f748ecb082f
Gordon Moore <gmgit@gordonmoore.co.uk>
2024-06-28 16:22:38 +0100

    Initial

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import (
-    Flask, redirect, render_template, request, flash, jsonify, send_file
+    Flask, redirect, render_template, flash, jsonify, request
 )
-from contacts_model import Contact, Archiver
-import time
+from contacts_model import Contact
 
 Contact.load_db()
 
@@ -23,52 +22,15 @@ def index():
 @app.route("/contacts")
 def contacts():
     search = request.args.get("q")
-    page = int(request.args.get("page", 1))
     if search is not None:
         contacts_set = Contact.search(search)
-        if request.headers.get('HX-Trigger') == 'search':
-            return render_template("rows.html", contacts=contacts_set)
     else:
         contacts_set = Contact.all()
-    return render_template("index.html", contacts=contacts_set, archiver=Archiver.get())
-
-
-@app.route("/contacts/archive", methods=["POST"])
-def start_archive():
-    archiver = Archiver.get()
-    archiver.run()
-    return render_template("archive_ui.html", archiver=archiver)
-
-
-@app.route("/contacts/archive", methods=["GET"])
-def archive_status():
-    archiver = Archiver.get()
-    return render_template("archive_ui.html", archiver=archiver)
-
-
-@app.route("/contacts/archive/file", methods=["GET"])
-def archive_content():
-    archiver = Archiver.get()
-    return send_file(archiver.archive_file(), "archive.json", as_attachment=True)
-
-
-@app.route("/contacts/archive", methods=["DELETE"])
-def reset_archive():
-    archiver = Archiver.get()
-    archiver.reset()
-    return render_template("archive_ui.html", archiver=archiver)
-
-
-@app.route("/contacts/count")
-def contacts_count():
-    count = Contact.count()
-    return "(" + str(count) + " total Contacts)"
-
+    return render_template("list.html", contacts=contacts_set)
 
 @app.route("/contacts/new", methods=['GET'])
 def contacts_new_get():
     return render_template("new.html", contact=Contact())
-
 
 @app.route("/contacts/new", methods=['POST'])
 def contacts_new():
@@ -79,7 +41,6 @@ def contacts_new():
         return redirect("/contacts")
     else:
         return render_template("new.html", contact=c)
-
 
 @app.route("/contacts/<contact_id>")
 def contacts_view(contact_id=0):
@@ -103,36 +64,17 @@ def contacts_edit_post(contact_id=0):
     else:
         return render_template("edit.html", contact=c)
 
-
-@app.route("/contacts/<contact_id>/email", methods=["GET"])
-def contacts_email_get(contact_id=0):
-    c = Contact.find(contact_id)
-    c.email = request.args.get('email')
-    c.validate()
-    return c.errors.get('email') or ""
-
-
-@app.route("/contacts/<contact_id>", methods=["DELETE"])
+@app.route("/contacts/<contact_id>/delete", methods=["GET"])
 def contacts_delete(contact_id=0):
     contact = Contact.find(contact_id)
     contact.delete()
-    if request.headers.get('HX-Trigger') == 'delete-btn':
-        flash("Deleted Contact!")
-        return redirect("/contacts", 303)
-    else:
-        return ""
+    flash("Deleted Contact!")
+    return redirect("/contacts")
 
-
-@app.route("/contacts/", methods=["DELETE"])
-def contacts_delete_all():
-    contact_ids = list(map(int, request.form.getlist("selected_contact_ids")))
-    for contact_id in contact_ids:
-        contact = Contact.find(contact_id)
-        contact.delete()
-    flash("Deleted Contacts!")
-    contacts_set = Contact.all(1)
-    return render_template("index.html", contacts=contacts_set)
-
+@app.route("/contacts/count")
+def contacts_count():
+    count = Contact.count()
+    return "(" + str(count) + " total Contacts)"
 
 # ===========================================================
 # JSON Data API
@@ -179,3 +121,6 @@ def json_contacts_delete(contact_id=0):
 
 if __name__ == "__main__":
     app.run()
+
+
+
